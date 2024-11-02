afb3b7796a5926436d5bb1d8bb4b419755274c8d
dot.box <Mr.Bears2013@gmail.com>
2024-11-02 13:56:46 -0400

    Made logo route back to home

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -2,6 +2,7 @@ import NavbarIcon from "@/app/ui/navbar-icons";
 import LogoIcon from "@/app/ui/logo-icons";
 import {RiAccountCircleLine} from "react-icons/ri";
 import {RiShoppingCartLine } from "react-icons/ri";
+import Link from 'next/link';
 import { BiLogoStripe } from "react-icons/bi";
 
 
@@ -10,9 +11,13 @@ export default function Navbar() {
         <div className="fixed left-0 right-0 w-15 m-5 p-3 flex bg-blue-500 rounded-lg shadow-2xl
         text-white ">
             <div className="flex items-center mr-auto">
+
                 <i className="mr-4">
+                    <Link href="/">
                     <LogoIcon icon={<BiLogoStripe size="28"/>}/>
+                    </Link>
                 </i>
+
             </div>
             <div className="flex items-center ">
                 <i className="mr-4">
