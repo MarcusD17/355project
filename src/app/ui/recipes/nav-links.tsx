98219e74e13afd20afe55a50db05af710d49551e
JahidHWW <106772217+JahidHWW@users.noreply.github.com>
2024-11-02 17:34:21 -0400

    updated navlinks

--- src/app/ui/recipes/nav-links.tsx
+++ src/app/ui/recipes/nav-links.tsx
@@ -14,8 +14,8 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-    { name: 'Home', href: '/dashboard', icon: HomeIcon },
-    { name: 'Recipes', href: '/recipes/recipes', icon: DocumentDuplicateIcon },
+    { name: 'Home', href: '/', icon: HomeIcon },
+    { name: 'Recipes', href: '/recipes/recipe-search', icon: DocumentDuplicateIcon },
     { name: 'Add Recipe', href: '/recipes/new-recipe', icon: DocumentDuplicateIcon },
     { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
