4ca874060295b406032805ce101a90d0adc511e8
JahidHWW <106772217+JahidHWW@users.noreply.github.com>
2024-11-02 17:08:08 -0400

    added nav links to navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,9 +1,10 @@
 import NavbarIcon from "@/app/ui/navbar-icons";
 import LogoIcon from "@/app/ui/logo-icons";
-import {RiAccountCircleLine} from "react-icons/ri";
-import {RiShoppingCartLine } from "react-icons/ri";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { RiShoppingCartLine } from "react-icons/ri";
 import Link from 'next/link';
 import { BiLogoStripe } from "react-icons/bi";
+import NavLinks from './recipes/nav-link';
 
 
 export default function Navbar() {
@@ -14,17 +15,18 @@ export default function Navbar() {
 
                 <i className="mr-4">
                     <Link href="/">
-                    <LogoIcon icon={<BiLogoStripe size="28"/>}/>
+                        <LogoIcon icon={<BiLogoStripe size="28" />} />
                     </Link>
                 </i>
 
             </div>
+            <NavLinks />
             <div className="flex items-center ">
                 <i className="mr-4">
-                    <NavbarIcon icon={<RiAccountCircleLine size="28"/>}/>
+                    <NavbarIcon icon={<RiAccountCircleLine size="28" />} />
                 </i>
                 <i className="mr-16">
-                    <NavbarIcon icon={<RiShoppingCartLine size="28"/>}/>
+                    <NavbarIcon icon={<RiShoppingCartLine size="28" />} />
                 </i>
             </div>
         </div>
