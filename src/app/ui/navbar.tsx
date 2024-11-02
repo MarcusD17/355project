4595f8597b3256dc89c2fc095e696daed929c0a9
JahidHWW <106772217+JahidHWW@users.noreply.github.com>
2024-11-02 18:34:11 -0400

    updated the navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,7 +1,7 @@
 import NavbarIcon from "@/app/ui/navbar-icons";
 import LogoIcon from "@/app/ui/logo-icons";
-// import { RiAccountCircleLine } from "react-icons/ri";
-// import { RiShoppingCartLine } from "react-icons/ri";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { RiShoppingCartLine } from "react-icons/ri";
 import Link from 'next/link';
 import { BiLogoStripe } from "react-icons/bi";
 import NavLinks from './recipes/nav-links';
@@ -20,14 +20,15 @@ export default function Navbar() {
 
             </div>
             <NavLinks />
-            <div className="flex items-center ">
+            {/* <div className="flex items-center ">
                 <i className="mr-4">
                     <NavbarIcon icon={<RiAccountCircleLine size="28" />} />
                 </i>
                 <i className="mr-16">
                     <NavbarIcon icon={<RiShoppingCartLine size="28" />} />
                 </i>
-            </div>
+            </div> */}
+
         </div>
 
     );
